cb38b7d2a2db0afac25891c360e40dda3e5eaa4a
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2021-12-03 14:11:29 -0500

    GET /books

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ const db = require('better-sqlite3')('database.db')
 app.use(express.static('./client/build'))
 app.use(express.json())
 
-app.get('/items')
+app.get('/books',(req,res) => {
+    const query = db.prepare("SELECT * FROM books")
+    const books = query.all()
+    res.json({
+        books
+    })
+})
+
+app.listen(3000,() => {
+    console.log("server started")
+})
