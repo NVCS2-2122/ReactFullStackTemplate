679c61a95171582feac4de1e5f4623ec2aa91fa9
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2021-12-03 14:42:38 -0500

    POST /checkout

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ app.get('/books',(req,res) => {
     })
 })
 
+app.post("/checkout",(req,res) => {
+    const {bookId} = req.body
+    const query = db.prepare("UPDATE books SET available = 0 WHERE id = ?")
+    const result = query.run(bookId)
+    console.log(`checked out ${result.changes} book(s)`)
+    res.json({
+        didCheckOut: result.changes > 0 ? true : false,
+        bookId
+    })
+})
+
 app.listen(3000,() => {
     console.log("server started")
 })
