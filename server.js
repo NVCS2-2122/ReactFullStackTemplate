e974d7cacaf79e18dfb24d50043b6f550e1befd5
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2021-12-03 14:43:11 -0500

    Server (#2)
    * GET /books
    
    * POST /checkout

--- server.js
+++ server.js
@@ -5,4 +5,25 @@ const db = require('better-sqlite3')('database.db')
 app.use(express.static('./client/build'))
 app.use(express.json())
 
-app.get('/items')
+app.get('/books',(req,res) => {
+    const query = db.prepare("SELECT * FROM books")
+    const books = query.all()
+    res.json({
+        books
+    })
+})
+
+app.post("/checkout",(req,res) => {
+    const {bookId} = req.body
+    const query = db.prepare("UPDATE books SET available = 0 WHERE id = ?")
+    const result = query.run(bookId)
+    console.log(`checked out ${result.changes} book(s)`)
+    res.json({
+        didCheckOut: result.changes > 0 ? true : false,
+        bookId
+    })
+})
+
+app.listen(3000,() => {
+    console.log("server started")
+})
