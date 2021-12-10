8ff822e8fb030c30769b18d48b4f3f37d52074e9
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2021-12-10 11:10:20 -0500

    handle book did not check out on server

---
+++ client/src/Error.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Error = () => {
+    return (
+        <div
+            style={{
+                backgroundColor: "red"
+            }}
+        >
+           Sorry, you cannot check out that book. 
+        </div>
+    )
+}
+
+export default Error
